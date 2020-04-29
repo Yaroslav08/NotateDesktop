550d9f80f8c5b2b18d9b1a4b3aeca44e619c5d2a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-04-29 12:43:02 +0300

    Added command for close app

--- NotateDesktop/NotateDesktop/ViewModels/Windows/WorkWindowViewModel.cs
+++ NotateDesktop/NotateDesktop/ViewModels/Windows/WorkWindowViewModel.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace NotateDesktop.ViewModels.Windows
 {
     public class WorkWindowViewModel
     {
         public WorkWindowViewModel() { }
+
+
+
+
+        public ICommand CloseWindow
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    Application.Current.Shutdown();
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
