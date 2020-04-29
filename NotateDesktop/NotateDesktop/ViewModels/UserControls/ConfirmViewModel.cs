52eca02cba1faf57150a3471be1bf4a0a6c65d68
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-04-29 12:21:41 +0300

    Added ViewModel for confirm

---
+++ NotateDesktop/NotateDesktop/ViewModels/UserControls/ConfirmViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotateDesktop.ViewModels.UserControls
+{
+    public class ConfirmViewModel
+    {
+        public ConfirmViewModel() { }
+        private string login;
+        private string code;
+    }
+}
