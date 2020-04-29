03dbdabcb9e9dc9045ed9e45f2265fef83fd48ab
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-04-29 13:19:44 +0300

    Created a style for button menu

--- NotateDesktop/NotateDesktop/Views/Windows/WorkWindow.xaml.cs
+++ NotateDesktop/NotateDesktop/Views/Windows/WorkWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace NotateDesktop.Views.Windows
             InitializeComponent();
             this.DataContext = new WorkWindowViewModel();
         }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
     }
 }
