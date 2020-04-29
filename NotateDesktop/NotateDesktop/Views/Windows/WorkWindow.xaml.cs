0c8c2bdcbe5b3f5f76cd556b96aa3c1b5cec45dd
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-04-29 15:21:20 +0300

    Added animation for open and close NavigateBar

--- NotateDesktop/NotateDesktop/Views/Windows/WorkWindow.xaml.cs
+++ NotateDesktop/NotateDesktop/Views/Windows/WorkWindow.xaml.cs
@@ -29,15 +29,5 @@ namespace NotateDesktop.Views.Windows
         {
             this.DragMove();
         }
-
-        private void btnMenuOpen_Click(object sender, RoutedEventArgs e)
-        {
-            gridMenu.Margin = new Thickness(0, 0, 0, 0);
-        }
-
-        private void btnMenuClose_Click(object sender, RoutedEventArgs e)
-        {
-            gridMenu.Margin = new Thickness(-250, 0, 0, 0);
-        }
     }
 }
