7810af7240ab0b10039b67dfbd00b4543ba5dd58
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-05-21 19:37:31 +0300

    Added CreateNote UserControl

---
+++ NotateDesktop/NotateDesktop/Views/UserControls/CreateNote.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotateDesktop.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CreateNote.xaml
+    /// </summary>
+    public partial class CreateNote : UserControl
+    {
+        public CreateNote()
+        {
+            InitializeComponent();
+        }
+    }
+}
